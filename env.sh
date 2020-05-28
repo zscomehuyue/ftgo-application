c95cffda417d13141d5b09bf01b19d0ad791a02a
zscomehuyue <zscomehuyue@126.com>
2020-05-28 20:28:23 +0800

    配置中间件执行脚本，mysql相关配置

--- env.sh
+++ env.sh
@@ -11,9 +11,6 @@ nohup sh /shell/workershell/zk.sh  &>env.log &
 nohup sh /shell/workershell/kafak.sh  &>env.log &
 nohup sh /shell/workershell/mongodb.sh  &>env.log &
 
-
-
-
 tail -f env.log
 
 
--- ftgo-restaurant-service/src/main/java/net/chrisrichardson/ftgo/restaurantservice/RestaurantServiceMain.java
+++ ftgo-restaurant-service/src/main/java/net/chrisrichardson/ftgo/restaurantservice/RestaurantServiceMain.java
@@ -5,17 +5,15 @@ import io.eventuate.common.json.mapper.JSonMapper;
 import io.eventuate.tram.jdbckafka.TramJdbcKafkaConfiguration;
 import net.chrisrichardson.eventstore.examples.customersandorders.commonswagger.CommonSwaggerConfiguration;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Primary;
 
 @SpringBootApplication
-@EnableAutoConfiguration
+//@EnableAutoConfiguration
 @Import({TramJdbcKafkaConfiguration.class, CommonSwaggerConfiguration.class})
-@ComponentScan
+//@ComponentScan
 public class RestaurantServiceMain {
 
   @Bean
