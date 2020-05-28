166b4d513c7b84ab8cffa27c3bd7cde476ab3452
zscomehuyue <zscomehuyue@126.com>
2020-05-28 20:30:05 +0800

    配置中间件执行脚本，mysql相关配置

--- sql.sql
+++ sql.sql
@@ -1,20 +1,20 @@
 create user 'order'@'*' identified by 'root';
-grant all privileges  on *.*  to "order"@'localhost' identified by 'root';
+grant all privileges  on *.*  to "order"@'%' identified by 'root';
 
 create user 'account'@'*' identified by 'root';
-grant all privileges  on *.*  to "account"@'localhost' identified by 'root';
+grant all privileges  on *.*  to "account"@'%' identified by 'root';
 
 create user 'consumer'@'*' identified by 'root';
-grant all privileges  on *.*  to "consumer"@'localhost' identified by 'root';
+grant all privileges  on *.*  to "consumer"@'%' identified by 'root';
 
 create user 'kitchen'@'*' identified by 'root';
-grant all privileges  on *.*  to "kitchen"@'localhost' identified by 'root';
+grant all privileges  on *.*  to "kitchen"@'%' identified by 'root';
 
 create user 'restaurant'@'*' identified by 'root';
-grant all privileges  on *.*  to "restaurant"@'localhost' identified by 'root';
+grant all privileges  on *.*  to "restaurant"@'%' identified by 'root';
 
 create user 'eventuate'@'*' identified by 'root';
-grant all privileges  on *.*  to "eventuate"@'localhost' identified by 'root';
+grant all privileges  on *.*  to "eventuate"@'%' identified by 'root';
 
 CREATE DATABASE IF NOT EXISTS ftgo_accounting_service DEFAULT CHARACTER SET utf8  DEFAULT COLLATE utf8_general_ci;
 CREATE DATABASE IF NOT EXISTS ftgo_order_service DEFAULT CHARACTER SET utf8  DEFAULT COLLATE utf8_general_ci;
